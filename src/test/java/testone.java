42c9a4a8c1155200e1ded176cb49c746215eda15
Sarah Usher <sarahnusher@gmail.com>
2020-12-16 17:24:40 +0000

    first commit

---
+++ src/test/java/testone.java
@@ -0,0 +1,10 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class testone {
+
+    @Test
+    public void checktest(){
+        Assertions.assertEquals(1,0);
+    }
+}
